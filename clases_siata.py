a1cd01666e13a0508f195c1e721dbb7acfa3821f
javacaru <janna.castaneda@udea.edu.co>
2026-04-26 19:47:01 -0500

    clase siata
    añado parte de atributos

--- clases_siata.py
+++ clases_siata.py
@@ -57,3 +57,42 @@ def validar_opcion(mensaje: str, opciones: list) -> str:
             return valor
         print(f"  ⚠  Opción inválida. Elija entre: {opciones}")
 
+
+#  _________________CLASE SiataCSV_________________
+
+
+class SiataCSV:
+  
+
+#Atributos
+#ruta:tipo str  – ruta completa del archivo
+#nombre:tipo str  – nombre del archivo sin extensión
+#df: tipo DataFrame – datos cargados con fecha_hora como índice
+# carpeta_g:tipo str  – carpeta donde se guardan los gráficos
+   
+
+    COLUMNAS_CALIDAD = {
+        "pm25": "PM2.5 (µg/m³)",
+        "pm10": "PM10 (µg/m³)",
+        "no":   "NO (ppb)",
+        "no2":  "NO₂ (ppb)",
+        "nox":  "NOx (ppb)",
+        "ozono":"Ozono (ppb)",
+        "co":   "CO (ppm)",
+    }
+
+    def __init__(self, ruta: str, carpeta_graficos: str = "graficos"):
+        if not os.path.isfile(ruta):
+            raise FileNotFoundError(f"No se encontró el archivo: {ruta}")
+        if not ruta.lower().endswith(".csv"):
+            raise ValueError("El archivo debe ser .csv")
+
+        self.ruta = ruta
+        self.nombre = os.path.splitext(os.path.basename(ruta))[0]
+        self.carpeta_g = carpeta_graficos
+        os.makedirs(self.carpeta_g, exist_ok=True)
+        #indice hora
+        self.df = pd.read_csv(ruta, parse_dates=["fecha_hora"])
+        self.df.set_index("fecha_hora", inplace=True)
+        print(f"  ✔  Archivo '{self.nombre}' cargado correctamente. "
+              f"({len(self.df)} registros, {len(self.df.columns)} columnas)")
